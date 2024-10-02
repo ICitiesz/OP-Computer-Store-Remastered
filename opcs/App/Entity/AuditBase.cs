2bddd2a0c832a6f7d01622be97f5a7017c44cd0f
ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>
2024-10-02 17:04:56 +0800

    - (Recommit) Added default value to `AuditBase`

--- opcs/App/Entity/AuditBase.cs
+++ opcs/App/Entity/AuditBase.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace opcs.App.Entity;
@@ -6,14 +5,14 @@ namespace opcs.App.Entity;
 public abstract class AuditBase
 {
     [Column("created_at", TypeName = "datetime")]
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     [Column("created_by", TypeName = "varchar(15)")]
-    public string CreatedBy { get; set; } = string.Empty;
+    public string CreatedBy { get; set; } = "SYSTEM";
 
     [Column("modified_at", TypeName = "datetime")]
-    public DateTime ModifiedAt { get; set; }
+    public DateTime ModifiedAt { get; set; } = DateTime.UtcNow;
 
     [Column("modified_by", TypeName = "varchar(15)")]
-    public string ModifiedBy { get; set; } = string.Empty;
+    public string ModifiedBy { get; set; } = "SYSTEM";
 }
